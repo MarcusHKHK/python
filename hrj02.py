2050902b2bf576f0c075f40120b03ab5c1d0a8ff
MarcusHKHK <marcus@hkhk.edu.ee>
2024-10-04 11:40:48 +0300

    hrj02 (9 ja 10)

---
+++ hrj02.py
@@ -0,0 +1,18 @@
+#Marcus Kruto
+#04.10.24
+#hrj02
+
+
+# 10. kütusekulu arvutamine
+
+liiter = int(input("Lisa kütusekulu: "))
+km = int(input("Lisa läbitud kilomeetrid: "))
+kytusekulu = liiter/(km/100)
+print("Sinu keskmine kütusekulu on: ", kytusekulu)
+
+
+#9. arvusüsteemid
+
+arv = int(input("Lisa 10nd arv: "))
+print(bin(arv))
+print(hex(arv))
