af261b505cb2c9593f6fc68d67dd9f381a8c14ca
MarcusHKHK <marcus@hkhk.edu.ee>
2024-10-04 11:18:03 +0300

    hrj01

---
+++ hrj01.py
@@ -0,0 +1,19 @@
+#Marcus Kruto
+#04.10.24
+#hrj01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print()
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^')
+print()
+
+#int=täisarv
+#str=string
+#float komaarv 1.23
+a = int(input("Lisa ruudu külje pikkus: "))
+s = a ** 2 #astendamine
+
+print("Vastus: ruudu pindala on ", s)
