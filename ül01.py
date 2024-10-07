fb5d8ef8a950e5a86cfc652e7c45c12cc05fa1f2
MarcusHKHK <marcus@hkhk.edu.ee>
2024-10-07 15:38:19 +0300

    Add files via upload

---
+++ ül01.py
@@ -0,0 +1,75 @@
+#Marcus Krutto IT-24
+#07.10.2024
+#Ül01
+
+#import turtle
+import turtle
+
+
+#Põhi kujund 4
+turtle.fd(200)
+turtle.left(90)
+turtle.fd(200)
+turtle.left(90)
+turtle.fd(200)
+turtle.left(90)
+turtle.fd(200)
+turtle.left(90)
+
+#Väiksed kujundid
+
+#1 väike kujund
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+
+#2 väike kujund
+
+turtle.right(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(100)
+turtle.right(90)
+turtle.fd(100)
+
+#3 väike kujund
+
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(150)
+turtle.left(90)
+turtle.fd(150)
+
+#sest on igav
+
+turtle.penup()
+turtle.fd(50)
+turtle.pendown()
+turtle.right(90)
+turtle.fd(100)
+turtle.right(90)
+turtle.fd(300)
+turtle.right(90)
+turtle.fd(300)
+turtle.right(90)
+turtle.fd(300)
+turtle.right(90)
+turtle.fd(300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
