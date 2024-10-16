4a318b6c91df1fcd46c2246c5c72c934f3c580dd
MarcusHKHK <marcus@hkhk.edu.ee>
2024-10-16 11:46:02 +0300

    Create hrj04.py

---
+++ hrj04.py
@@ -0,0 +1,20 @@
+#16.10.24 Marcus Krutto
+#hrj04
+
+"""
+#1. aia pikkus
+a = int(input("Sisesta külg 1: "))
+b = int(input("Sisesta külg 2: "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus on {p} meetrit.")
+"""
+
+#Raamatu allahindlus
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus
+print(f"{kogus} raamatu hind soodustega on {summa:0.2f}€.")
+
+
+
