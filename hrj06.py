12c4bf9b710792c78056f652647dd7086fb06a5a
MarcusHKHK <marcus@hkhk.edu.ee>
2024-10-18 11:47:25 +0300

    Create hrj06.py

---
+++ hrj06.py
@@ -0,0 +1,33 @@
+#18.10.2024 Marcus Krutto
+#Ülesanne 6
+
+#Import
+import turtle
+import math
+
+#Redeli joonestamine maja seinast
+kordaja = 10
+a = math.radians(53)
+h = 4.4
+x = abs (h / math.tan(a))
+c = math.sqrt(h**2 + x**2)
+
+
+print(x)
+
+turtle.fd(x * kordaja)
+turtle.lt(90)
+turtle.fd(h * kordaja)
+turtle.lt(180-37)
+turtle.fd(c * kordaja)
+
+
+
+
+
+
+
+
+
+turtle.done()
+
