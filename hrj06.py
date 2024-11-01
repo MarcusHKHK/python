cd73ee31b69b70790d1eb81126fbb85ffe548f32
MarcusHKHK <marcus@hkhk.edu.ee>
2024-11-01 14:49:38 +0200

    Update hrj06.py

--- hrj06.py
+++ hrj06.py
@@ -12,20 +12,12 @@ h = 4.4
 x = abs (h / math.tan(a))
 c = math.sqrt(h**2 + x**2)
 
-
-print(x)
-
+#Turtle liikumine
 turtle.fd(x * kordaja)
 turtle.lt(90)
 turtle.fd(h * kordaja)
 turtle.lt(180-37)
 turtle.fd(c * kordaja)
-
-
-
-
-
-
 
 
 
