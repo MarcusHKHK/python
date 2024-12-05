f7500af93df1ca12ebcc90e4cede4d634cb376a6
MarcusHKHK <marcus@hkhk.edu.ee>
2024-12-05 11:14:56 +0200

    tere

---
+++ isetöö1.py
@@ -0,0 +1,65 @@
+#Marcus Krutto 15.5.2024
+#Iseseisev töö
+
+
+# print("Tere, maailm!")
+
+# aasta = 2020
+# liblikas = "teelehe-mosaiikliblikas"
+# lause_keskosa = "aasta liblikas on"
+# lause = aasta, lause_keskosa, liblikas
+# print(lause)
+
+#Pilved
+
+# vastus = float(input("Pilvede aluse kõrgus (kilomeetrites): "))
+# number = float()
+# if number>=6.0:
+#     print("Need on ülemised pilved!")
+# else:
+#     print("Need ei ole ülemised pilved!")
+
+
+# #bussid
+# print("Palun sisestada ainult täisarvud!")
+# inimesed = int(input("Mitu inimest on vaja transpordida: "))
+# kohtade_arv = int(input("Mitu istekohta on bussis: "))
+
+# a = inimesed//kohtade_arv
+# b = inimesed%kohtade_arv
+# if inimesed==kohtade_arv:
+#     print(f"Selle arvu inimeste transportimiseks on vaja {a} arv busse, viimases bussis istub {a}")
+# else:
+#     print(f"Selle arvu inimeste transportimiseks on vaja {a} arv busse, viimases bussis istub {b}")
+
+
+# #Hommik
+
+# a = int(input("Mitu korda on vaja äratada: "))
+# for i in range(a):
+#     print("On hommik, aeg on üles tõusta")
+
+
+#Jänese majustused
+ringide_arv = 4
+i = 1
+porgandite_arv = 0
+while i<=ringide_arv:
+    if i%2==0:
+        porgandite_arv + i
+        print(i)
+    i =+ 1
+print(f"Porgandite koguarv on: {porgandite_arv}")
+
+
+
+
+
+
+
+
+
+
+
+
+
