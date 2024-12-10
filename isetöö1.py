53520e0f43dda57699c38dd66e3bf2829dde58de
MarcusHKHK <marcus@hkhk.edu.ee>
2024-12-10 09:58:31 +0200

    Update isetöö1.py

--- isetöö1.py
+++ isetöö1.py
@@ -40,16 +40,47 @@
 #     print("On hommik, aeg on üles tõusta")
 
 
-#Jänese majustused
-ringide_arv = 4
-i = 1
-porgandite_arv = 0
-while i<=ringide_arv:
-    if i%2==0:
-        porgandite_arv + i
-        print(i)
-    i =+ 1
-print(f"Porgandite koguarv on: {porgandite_arv}")
+# #Jänese majustused
+# ringide_arv = 4
+# i = 1
+# porgandite_arv = 0
+# while i<=ringide_arv:
+#     if i%2==0:
+#         porgandite_arv + i
+#         print(i)
+#     i =+ 1
+# print(f"Porgandite koguarv on: {porgandite_arv}")
+
+
+#Täringud
+# taring = int(input("Mitu täringut on vaja visata: "))
+# for i in range(taring):
+#      a = random.randrange(1,6)
+#      print(a)
+
+
+#Male
+# i = 0
+
+# while i==0:
+#     taisarv = int(input("Palun sisesta üks täisarv: "))
+#     if taisarv == 1:
+#         print(f"Nisuteri {taisarv}. ruudu eest saab: 1")
+#         i+1
+#     else:
+#         a = taisarv-1
+#         print(f"Nisuteri {taisarv}. ruudu eest saab: {2**a}")
+#         i+1
+
+#Õunad
+poialpoiss = int(input("Mitu pöialpoissi soovib õuna (1-7): "))
+b = []
+for i in range(poialpoiss):
+    a = random.randint(1,2)
+    print(a)
+    b.append(a)
+print(f"Lumivalgekesele jäi {14-sum(b)} arv õuna")
+
 
 
 
