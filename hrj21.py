35322a832836d355a46e7716fca0351f954934e8
MarcusHKHK <marcus@hkhk.edu.ee>
2025-01-15 11:16:43 +0200

    harjutused

---
+++ hrj21.py
@@ -0,0 +1,35 @@
+#Marcus Krutto Hrj21
+#14.01.25
+
+import os
+from PIL import Image, ImageOps
+
+def create_thumbnails(source_dir, thumb_dir, size=(350, 350)):
+    nimi = 0
+    # Kontrolli, kas thumb kataloog on olemas, kui ei ole, siis loo see
+    if not os.path.exists(thumb_dir):
+        os.makedirs(thumb_dir)
+
+    # Kõikide JPG failide leidmine kataloogis
+    for filename in os.listdir(source_dir):
+        if filename.lower().endswith(".jpg"):
+            nimi+=1
+            img_path = os.path.join(source_dir, filename)
+            img = Image.open(img_path)
+           
+            # Pisipildi loomine
+            img = img.rotate(90)
+            img = img.convert('L')
+            thumb_img = ImageOps.fit(img, size, centering=(0.5, 0.5))
+            img.close()
+           
+            # Pisipildi salvestamine
+            thumb_path = os.path.join(thumb_dir, str(nimi)+".jpg")
+            thumb_img.save(thumb_path, "JPEG")
+           
+            print(f"Pisipilt salvestatud: {thumb_path}")
+
+# Kasuta funktsiooni
+source_directory = 'pildid'
+thumb_directory = 'tööpildid'
+create_thumbnails(source_directory, thumb_directory)
---
+++ hrj22.py
@@ -0,0 +1,15 @@
+#Marcus Krutto 15.01.25
+#Harjutus 22
+
+import dateparser
+from datetime import datetime
+
+# Kuva praegune päev, kuu, aasta, tund, minut
+# Vorminda praegune kuupäev järgmiselt: d.m Y,  H:M:S
+# Lisa oma sünniaeg, arvuta ja kuva, mitu päeva vana oled
+# Kuva vanus aastates
+# Kuva, kas tegemist on juubeliaastaga
+
+datetime_object = datetime.now()
+
+print(datetime_object)
