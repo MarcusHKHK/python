ef849aee8ef4a4be86867de076f93115f794c98e
MarcusHKHK <marcus@hkhk.edu.ee>
2025-01-15 11:43:01 +0200

    Update hrj22.py

--- hrj22.py
+++ hrj22.py
@@ -2,7 +2,6 @@
 #Harjutus 22
 
 from datetime import datetime, timedelta
-
 import dateparser
 
 # Kuva praegune päev, kuu, aasta, tund, minut
@@ -11,6 +10,19 @@ import dateparser
 # Kuva vanus aastates
 # Kuva, kas tegemist on juubeliaastaga
 
-sp = datetime(2008, 1, 14)
-now = datetime.now
+sp = datetime(2010, 1, 14)
+now = datetime.now()
 print(now)
+print(now.strftime("%d.%m.%Y %H:%M:%S"))
+
+#Vanus
+
+vanus = now - sp
+print("Minu vanus: ", vanus.days)
+print("Minu vanus: ", now.year-sp.year)
+
+#Juubel
+if int(vanus.days/365)%5==0:
+    print("See on juubeli aasta.")
+else:
+    print("See ei ole juubeli aasta.")
